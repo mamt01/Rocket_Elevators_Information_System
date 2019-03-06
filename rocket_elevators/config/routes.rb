d7fd3e9cbf01ce8b185059bd5a823386207af1e5
Andrew Roy <andrewroy27@outlook.com>
2019-03-06 15:18:40 -0500

    Commercial Done

--- rocket_elevators/config/routes.rb
+++ rocket_elevators/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/residential', to: 'pages#residential'
   get '/commercial', to: 'pages#commercial'
   get '/index', to: 'pages#index'
+  get '/work1', to: 'pages#work1'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
