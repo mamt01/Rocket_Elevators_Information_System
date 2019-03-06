713ae1af17b7314bae34ef7853c2b1f6b9d82413
Tsitchom <raphael.vallee28@gmail.com>
2019-03-06 17:11:49 -0500

    5PM commit Wednesday

--- rocket_elevators/config/routes.rb
+++ rocket_elevators/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get '/gform', to: 'pages#gform'
   get '/residential', to: 'pages#residential'
   get '/commercial', to: 'pages#commercial'
+  get '/index', to: 'pages#index'
+  get '/re-admin-login', to: 'pages#re-admin-login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
