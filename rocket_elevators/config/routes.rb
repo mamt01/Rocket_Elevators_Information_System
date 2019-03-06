2711d7aa511512ef332796d1acd1b74f84814fc7
Andrew Roy <andrewroy27@outlook.com>
2019-03-06 15:02:23 -0500

    Commercial link done

--- rocket_elevators/config/routes.rb
+++ rocket_elevators/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/gform', to: 'pages#gform'
   get '/residential', to: 'pages#residential'
   get '/commercial', to: 'pages#commercial'
+  get '/index', to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
