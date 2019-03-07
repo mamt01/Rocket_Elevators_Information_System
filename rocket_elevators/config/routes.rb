92ea0c0036e978533475ff5edfa325c4151c8ea6
Tsitchom <raphael.vallee28@gmail.com>
2019-03-07 08:10:04 -0500

    Thursday morning full employee table push

--- rocket_elevators/config/routes.rb
+++ rocket_elevators/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'employees/new'
+  mount RailsAdmin::Engine => '/emp-dashboard', as: 'rails_admin'
   get '/gform', to: 'pages#gform'
   get '/residential', to: 'pages#residential'
   get '/commercial', to: 'pages#commercial'
   get '/index', to: 'pages#index'
   get '/re-admin-login', to: 'pages#re-admin-login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  root 'pages#index' #no to: before
 end
---
+++ rocket_elevators/db/migrate/20190307003223_create_employees.rb
@@ -0,0 +1,13 @@
+class CreateEmployees < ActiveRecord::Migration[5.2]
+  def change
+    create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :title
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
