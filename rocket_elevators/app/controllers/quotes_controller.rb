0e6732a9acee1f13b1c1711a4931a53a49adc765
Marc-Olivier Groleau <marcoliviergroleau@gmail.com>
2019-03-07 15:23:04 -0500

    Jeudi 15h

--- rocket_elevators/app/controllers/quotes_controller.rb
+++ rocket_elevators/app/controllers/quotes_controller.rb
@@ -25,38 +25,41 @@ class QuotesController < ApplicationController
   # POST /quotes.json
   def create
 
-
     @quote = Quote.new(quote_params)
 
+    puts "======================================="
+    puts params[:quote][:department]
+    puts @quote.inspect
+    puts "===================================="
     if params[:quote][:department] == 'Residential'
-      @quote.number_of_apartments = params(resi_number_of_apartments)
-      @quote.number_of_floors = params(resi_number_of_floors)
-      @quote.number_of_basements = params(resi_number_of_basements)
+      @quote.number_of_apartments = params[:resi_number_of_apartments]
+      @quote.number_of_floors = params[:resi_number_of_floors]
+      @quote.number_of_basements = params[:resi_number_of_basements]
     
-    elsif params[:quote][:departement] == 'Commercial' 
-      @quote.number_of_apartments = params(comm_number_of_apartments)
-      @quote.number_of_parking = params(comm_number_of_parking)
-      @quote.number_of_basements = params(comm_number_of_basements)
-      @quote.number_of_stores = params(comm_number_of_stores)
+    elsif params[:quote][:department] == 'Commercial' 
+      @quote.number_of_apartments = params[:comm_number_of_apartments]
+      @quote.number_of_parking = params[:comm_number_of_parking]
+      @quote.number_of_basements = params[:comm_number_of_basements]
+      @quote.number_of_stores = params[:comm_number_of_stores]
 
-    elsif params[:quote][:departement] == 'Corporate' 
-      @quote.number_of_floors = params(corp_number_of_floors)
-      @quote.number_of_basements = params(corp_number_of_basements)
-      @quote.max_occupancy_per_floor = params(corp_max_occupancy_per_floor)
-      @quote.number_of_stores = params(corp_number_of_stores)
-      @quote.number_of_parking = params(corp_number_of_parking)
+    elsif params[:quote][:department] == 'Corporate' 
+      @quote.number_of_floors = params[:corp_number_of_floors]
+      @quote.number_of_basements = params[:corp_number_of_basements]
+      @quote.max_occupancy_per_floor = params[:corp_max_occupancy_per_floor]
+      @quote.number_of_stores = params[:corp_number_of_stores]
+      @quote.number_of_parking = params[:corp_number_of_parking]
 
-    elsif params[:quote][:departement] == 'Hybrid' 
-      @quote.number_of_floors = params(hyb_number_of_floors)
-      @quote.number_of_basements = params(hyb_number_of_basements)
-      @quote.max_occupancy_per_floor = params(hyb_max_occupancy_per_floor)
-      @quote.number_of_stores = params(hyb_number_of_stores)
-      @quote.number_of_parking = params(hyb_number_of_parking)
+    elsif params[:quote][:department] == 'Hybrid' 
+      @quote.number_of_floors = params[:hyb_number_of_floors]
+      @quote.number_of_basements = params[:hyb_number_of_basements]
+      @quote.max_occupancy_per_floor = params[:hyb_max_occupancy_per_floor]
+      @quote.number_of_stores = params[:hyb_number_of_stores]
+      @quote.number_of_parking = params[:hyb_number_of_parking]
     end
 
     respond_to do |format|
       if @quote.save
-        format.html { redirect_to @quote, notice: 'Quote was successfully created.' }
+        format.html { redirect_to "http://localhost:3000/gform.html", notice: 'Quote was successfully created.' }
         format.json { render :show, status: :created, location: @quote }
       else
         format.html { render :new }
@@ -97,6 +100,6 @@ class QuotesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def quote_params
-      params.require(:quote).permit(:full_name, :company_name, :email, :phone_number, :number_of_floors, :number_of_basements, :number_of_parking, :number_of_stores, :max_occupancy_per_floor, :hours_of_activity, :service_level, :number_of_elevators, :installation_cost, :total_cost)
+      params.require(:quote).permit(:department, :full_name, :company_name, :email, :phone_number, :number_of_floors, :number_of_basements, :number_of_parking, :number_of_stores, :max_occupancy_per_floor, :hours_of_activity, :service_level, :number_of_elevators, :installation_cost, :total_cost)
     end
 end
