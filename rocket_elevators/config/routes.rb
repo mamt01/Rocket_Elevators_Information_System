7b0aa1ce8e1ed0a4f464d228a8ac0a641bfe4e97
Andrew Roy <andrewroy27@outlook.com>
2019-03-07 13:27:45 -0500

    Working Database

--- rocket_elevators/config/routes.rb
+++ rocket_elevators/config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   mount RailsAdmin::Engine => '/emp-dashboard', as: 'rails_admin'
   get '/gform', to: 'pages#gform'
   get '/residential', to: 'pages#residential'
   get '/commercial', to: 'pages#commercial'
   get '/index', to: 'pages#index'
   get '/re-admin-login', to: 'pages#re-admin-login'
-  get '/emp-dashboard', to: 'pages#emp-dashboard'
+  get '/rails_admin', to: 'pages#rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
