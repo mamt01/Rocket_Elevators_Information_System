0a669ba02b349ef6e52bad387a0474f1c0018148
MAMT01 <marc-antoine.m-tanguay.1@ulaval.ca>
2019-03-08 18:09:20 -0500

    regler column user problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_200244) do
+ActiveRecord::Schema.define(version: 2019_03_08_222222) do
 
   create_table "quotes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "department"
