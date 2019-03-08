e78ddfb22377ef6d5fe5bb3ab8796f98dac4c2cc
Andrew Roy <andrewroy27@outlook.com>
2019-03-08 10:11:04 -0500

    Loggout is down

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   get '/index', to: 'pages#index'
   get '/re-admin-login', to: 'pages#re-admin-login'
   get '/rails_admin', to: 'pages#rails_admin'
-  get '/users/sign_in', to: 'devise/sessions#new'
+  get '/users/sign_in', to: 'pages#emp-dashboard'
+  get '/users/sign_out', to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
