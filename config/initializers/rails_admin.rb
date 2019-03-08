f91ac9fba8bb917369de05d44f70437afa9e3f0b
Andrew Roy <andrewroy27@outlook.com>
2019-03-08 08:38:03 -0500

    Register and forgot password deleted

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -33,7 +33,7 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
-
+    config.current_user_method(&:current_user)
     ## With an audit adapter, you can add:
     # history_index
     # history_show
