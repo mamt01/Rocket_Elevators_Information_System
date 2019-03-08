b2a28a94c5574c809220fd0d804baa3be10a2884
MAMT01 <marc-antoine.m-tanguay.1@ulaval.ca>
2019-03-08 18:00:47 -0500

    final version

--- db/migrate/20190308200244_remove_reset_password_sent_at_from_users.rb
+++ db/migrate/20190308200244_remove_reset_password_sent_at_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveResetPasswordSentAtFromUsers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :reset_password_sent_at, :string
+    add_column :users, :reset_password_sent_at, :string
   end
 end
