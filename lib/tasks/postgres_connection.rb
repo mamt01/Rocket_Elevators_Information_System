e6e18f535f6887f52599b93b5286f8a76457122b
Tsitchom <raphael.vallee28@gmail.com>
2019-03-12 16:35:20 -0400

    fin PM andrew pull after postgres PM

--- lib/tasks/postgres_connection.rb
+++ lib/tasks/postgres_connection.rb
@@ -1,3 +1,3 @@
 require 'pg'
-conn = PG::Connection.open(:dbname => 'test')
+conn = PG::Connection.open(:rocket_elevator_development => 'test')
 res = conn.exec_params('SELECT $1 AS a, $2 AS b, $3 AS c', [1, 2, nil])
