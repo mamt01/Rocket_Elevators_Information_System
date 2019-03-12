852b38564fef9fe61068a14373b526f567dd8e7b
Andrew Roy <andrewroy27@outlook.com>
2019-03-12 16:20:18 -0400

    DB Populated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_12_172624) do
+ActiveRecord::Schema.define(version: 2019_03_12_135450) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "address_type", null: false
@@ -70,9 +70,9 @@ ActiveRecord::Schema.define(version: 2019_03_12_172624) do
 
   create_table "customers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "address_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.date "date_of_creation"
-    t.string "company_name"
+    t.string "company_name", null: false
     t.string "full_name_contact_person"
     t.string "phone_number_contact_person"
     t.string "email_contact_person"
