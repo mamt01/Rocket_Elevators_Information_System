b00ac41a67c69215556ceb32e3ed2d87f8ca6092
MAMT01 <marc-antoine.m-tanguay.1@ulaval.ca>
2019-03-14 15:59:40 -0400

    pg_connection.rake updated for postgre

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,5 @@
 class Quote < ApplicationRecord
+    validates :full_name, presence: true
     validates :number_of_elevators, presence: true 
     validates :service_level, presence: true 
     validates :total_cost, presence: true 
